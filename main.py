dd5e77e51437663d7b185b8b467440b9d4938d13
Stevedow99 <steve.dowling28@gmail.com>
2022-09-21 12:42:19 -0400

    updates

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import os
 import requests 
 
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # getting all of my inputs for use in the action
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 # setting the dbt cloud token to use dbt cloud API
 dbt_cloud_token = os.environ["INPUT_DBT_CLOUD_TOKEN"]
@@ -21,9 +21,9 @@ dbt_cloud_job_id = os.environ["INPUT_DBT_CLOUD_JOB_ID"]
 # # setting the failure_check_on_job_error
 # fail_check_on_job_error = os.environ["INPUT_FAIL_CHECK_ON_JOB_ERROR"]
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # use environment variables to set dbt cloud api configuration
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 # setting the headers for the dbt cloud api request
 req_auth_headers = {'Authorization': f'Token {dbt_cloud_token}'}
@@ -41,9 +41,10 @@ run_status_map = { # dbt run statuses are encoded as integers. This map provides
   30: 'Cancelled',
 }
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # setting a function to return the most recent run for a given job
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+
 
 def get_most_recent_run_for_job(base_url, headers, job_id):
 
@@ -56,28 +57,31 @@ def get_most_recent_run_for_job(base_url, headers, job_id):
     # returning the run id
     return most_recent_run_id
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # setting a function to return the status of the given job
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
-def get_run_status(base_url, headers, run_id):
-    
+
+def get_run_status(base_url, headers, run_id): 
+
     # setting the request url
     dbt_cloud_run_status_url = f"{base_url}/runs/{run_id}"
-    
+
     # get status
     run_status_code = requests.get(dbt_cloud_run_status_url, headers=headers).json()['data']['status']
-    
+
     # get status mapping
     run_status = run_status_map[run_status_code]
-    
+
     # returning the run status
     return run_status
 
+# ------------------------------------------------------------------------------
+# running the main function
+# ------------------------------------------------------------------------------
 
 
 def main():
-
     # getting the run id of the most recent run of the given job
     most_recent_run_id = get_most_recent_run_for_job(base_url=base_dbt_cloud_api_url, headers=req_auth_headers, job_id=dbt_cloud_job_id)
 
